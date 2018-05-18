c3c70afaeac36d80a97bd0c37f337d3e3d705f2f
Tironemota <38574802+Tironemota@users.noreply.github.com>
2018-05-18 12:53:47 -0400

    Consultas a la base de datos
    Consultas a la base de datos

---
+++ Curso PHP/consulta.php
@@ -0,0 +1,55 @@
+<?php
+include('conex.php');
+
+class ClaseBaseDatos
+{
+	private $objetoBase = '';
+	public function __construct()
+	{
+		$this->objetoBase = new mysqli(SERVER,USUARIO,PASS,NOMBREBASE);
+		if($this->objetoBase->connect_errno)
+		{
+			echo "Fallo la conexion: ".$objetoMysqli->connect_error;
+		}
+	}
+	public function listaEditor()
+	{
+		$query = "SELECT email FROM persona";
+		$b = '';
+		if($result = $this->objetoBase->query($query))
+		{
+			while($a = $result->fetch_assoc())
+			{
+				$b[] = $a;
+			}
+			return $b;
+		}
+	}
+	public function listaNoticia()
+	{
+		$query = "SELECT * FROM noticia";
+		$b = '';
+		if($result = $this->objetoBase->query($query))
+		{
+			while($a = $result->fetch_assoc())
+			{
+				$b[] = $a;
+			}
+			return $b;
+		}
+	}
+	public function detalleNoticia()
+	{
+		$id = $_GET['id'];
+		// SOLUCIONA PROBLEMAS DE INYECCION DE SQL
+		$id = intval($id);
+		$query = "SELECT email_autor,fechahora,titulo,contenido FROM noticia WHERE id = $id";
+		$a = '';
+		if($result = $this->objetoBase->query($query))
+		{
+			$a = $result->fetch_assoc(); 
+			
+		}
+		return $a;
+	}
+}
