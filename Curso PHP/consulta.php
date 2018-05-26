ea2da79f8bef2bfd3968bb3569740f0001f720a5
Tironemota <38574802+Tironemota@users.noreply.github.com>
2018-05-26 04:54:46 -0400

    Consultas a la base de datos
    Modificado el 26/05/2018

--- Curso PHP/consulta.php
+++ Curso PHP/consulta.php
@@ -41,8 +41,8 @@ class ClaseBaseDatos
 	public function detalleNoticia()
 	{
 		$id = $_GET['id'];
-		// SOLUCIONA PROBLEMAS DE INYECCION DE SQL
-		// CONVIERTE A VALOR NUMERICO LO QUE PASA POR EL PARAMETRO
+		// SOLUCIONA PROBLEMAS DE INYECCION DE SQL.
+		// CONVIERTE A VALOR NUMERICO LO QUE PASA POR EL PARAMETRO.
 		$id = intval($id);
 		$query = "SELECT email_autor,fechahora,titulo,contenido FROM noticia WHERE id = $id";
 		$a = '';
